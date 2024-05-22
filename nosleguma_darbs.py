f6eb15b9c051af3c643b2b318341e8e3b7a9def1
Kr1st0f3rs <165793796+Kr1st0f3rs@users.noreply.github.com>
2024-05-22 19:34:36 +0300

    Add files via upload

---
+++ nosleguma_darbs.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+import time
+
+#logs
+platums=720
+augstums=480
+logs=Tk()
+logs.title("Skolas zvans")
+canvas = Canvas(logs, width=platums, height=augstums)
+
+logs.mainloop()
